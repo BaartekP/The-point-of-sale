80d734a6647c8cf695b1496092b05ec3eef4e306
bartek <bartek.praselski@outlook.com>
2019-05-06 19:53:11 +0200

    initial commit of Sale

---
+++ src/Sale.java
@@ -0,0 +1,18 @@
+
+public class Sale {
+
+	private int sale_id;
+	private int salesman_id;
+	private double summ;
+	
+	public int get_sale_id() 		{return this.sale_id;}
+	public int get_salesman_id() 	{return this.salesman_id;}
+	public double get_summ() 		{return this.summ;}
+	
+	public void set_sale_id(int sale_id) 			{this.sale_id = sale_id;}
+	public void set_salesman_id(int salesman_id) 	{this.salesman_id = salesman_id;}
+	public void set_summ(double summ) 				{this.summ = summ;}
+	
+	
+	
+}
