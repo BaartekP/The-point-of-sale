53b1ecefaae098d54c372582fd351736ac5e1244
bartek <bartek.praselski@outlook.com>
2019-05-07 00:32:36 +0200

    beautify

--- src/Products.java
+++ src/Products.java
@@ -5,27 +5,15 @@ public class Products {
 	private String barcode;
 	private double price;
 	
-	
 	//Get
-	public String get_name() {
-		return this.name;
-	}
-	public String get_barcode() {
-		return this.barcode;
-	}
-	public double get_price() {
-		return this.price;
-	}
+	public String get_name() 		{return this.name;}
+	public String get_barcode() 	{return this.barcode;}
+	public double get_price() 		{return this.price;}
+	
 	//Set
-	public void set_name(String name) {
-		this.name = name;
-	}
-	public void set_barcode(String barcode) {
-		this.barcode = barcode;
-	}
-	public void set_price(double price) {
-		this.price = price;
-	}
+	public void set_name(String name) 		{this.name = name;}
+	public void set_barcode(String barcode) {this.barcode = barcode;}
+	public void set_price(double price) 	{this.price = price;}
 	
 	
 }
--- src/Sale.java
+++ src/Sale.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
+//import java.util.List;
+//import java.util.ArrayList;
 
 public class Sale {
 	
@@ -10,33 +10,33 @@ public class Sale {
 	private int sale_id;
 	private int salesman_id;
 	private double summ;
-	private  List<Products> productList;
-	private double[] amount; // constructor: 1
+	//private  List<Products> productList;
+	private double[] amount;
 	
-	
-	public int get_sale_id() 		{return this.sale_id;}
+	//Get
+	public int get_sale_id() 				{return this.sale_id;}
 	public double get_amount(int i) 		{return this.amount[i];}
-	public int get_salesman_id() 	{return this.salesman_id;}
-	public double get_summ() 		{return this.summ;}
-	public  List<Products> get_productList() {return this.productList;}
+	public int get_salesman_id() 			{return this.salesman_id;}
+	public double get_summ() 				{return this.summ;}
+	//public  List<Products> get_productList() {return this.productList;}
 	
+	//Set
 	public void set_sale_id(int sale_id) 			{this.sale_id = sale_id;}
 	public void set_amount(double[] amount) 		{this.amount = amount;}
 	public void set_salesman_id(int salesman_id) 	{this.salesman_id = salesman_id;}
 	public void set_summ(double summ) 				{this.summ = summ;}
-	public void set_productList(List<Products> p) 	{this.productList=p;}
+	//public void set_productList(List<Products> p) 	{this.productList=p;}
 	
-	public void newSale() {
-		
+	public void newSale() {	
 			
 			Products p = new Products();
-			List<Products> temp = new ArrayList<Products>();
+			//List<Products> temp = new ArrayList<Products>();
 			
-			// wpisz kod
+			// Barcode
 			System.out.print("Barcode:");
 			p.set_barcode(scan.next());
 			
-			// znajdz produkt w pliku
+			// Find the product
 			try {
             File file = new File("products.txt");
             Scanner scan = new Scanner(file);
@@ -48,7 +48,7 @@ public class Sale {
             		String[] parts = x.split(";");
             		p.set_barcode(parts[0]);
             		p.set_name(parts[1]);
-            		set_summ(Double.valueOf(parts[2]));
+            		p.set_price(Double.valueOf(parts[2]));
             	}            	
             }            
 			}catch(Exception e){
@@ -57,12 +57,8 @@ public class Sale {
 			System.out.print(p.get_name()+"\t"+p.get_price());
 		
 		
-		// wczytaj dane
-		// dodaj do ogólnego zamówienia
-		// zlicz sume
-		// wydrukuj na LCD
-		
-		
-	}
-	
+		// Final order
+		// Summ
+		// Print LCD and Printer	
+	}	
 }
