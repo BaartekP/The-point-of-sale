55671f9295d8ba1431911c1027fee286488efa59
bartek <bartek.praselski@outlook.com>
2019-05-08 14:57:17 +0200

    creating vector of products and quantities

--- src/Sale.java
+++ src/Sale.java
@@ -1,7 +1,8 @@
 import java.io.File;
 import java.util.Scanner;
-//import java.util.List;
-//import java.util.ArrayList;
+import java.util.Vector;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Sale {
 	
@@ -9,40 +10,40 @@ public class Sale {
 	
 	private int sale_id;
 	private int salesman_id;
-	private double summ;
-	//private  List<Products> productList;
-	private double[] amount;
+	private double summ = 0;
+	private List<Products> productList;
+	private Vector<Double> amount;
 	
 	//Get
 	public int get_sale_id() 				{return this.sale_id;}
-	public double get_amount(int i) 		{return this.amount[i];}
+	public double get_amount(int i)			{return this.amount.get(i);}
 	public int get_salesman_id() 			{return this.salesman_id;}
 	public double get_summ() 				{return this.summ;}
-	//public  List<Products> get_productList() {return this.productList;}
+	public List<Products> get_productList() {return this.productList;}
 	
 	//Set
 	public void set_sale_id(int sale_id) 			{this.sale_id = sale_id;}
-	public void set_amount(double[] amount) 		{this.amount = amount;}
+	public void set_amount(double amount) 			{this.amount.add(amount);}
 	public void set_salesman_id(int salesman_id) 	{this.salesman_id = salesman_id;}
 	public void set_summ(double summ) 				{this.summ = summ;}
-	//public void set_productList(List<Products> p) 	{this.productList=p;}
+	public void set_productList(Products p) 		{this.productList.add(p);}
 	
 	public void newSale() {	
-			
+		
+			this.amount = new Vector<Double>();
 			Products p = new Products();
-			//List<Products> temp = new ArrayList<Products>();
+			List<Products> temp = new ArrayList<Products>();
 			
-			// Barcode
 			System.out.print("Barcode:");
 			p.set_barcode(scan.next());
 			
 			// Find the product
 			try{
             File file = new File("products.txt");
-            Scanner scan = new Scanner(file);
+            Scanner scan1 = new Scanner(file);
             
-            while(scan.hasNextLine()) {
-            	String x = scan.next();
+            while(scan1.hasNextLine()) {
+            	String x = scan1.next();
             	if(x.contains(p.get_barcode())) {
             		//System.out.println(x);
             		String[] parts = x.split(";");
@@ -51,15 +52,24 @@ public class Sale {
             		p.set_price(Double.valueOf(parts[2]));
             		
             	}            	
-            }            
+            }
+            scan1.close();
 			}catch(Exception e){
-				System.err.println("Error: " + e.getMessage());
+				//System.err.println("Error: " + e.getMessage());
 			}
+			
+			temp.add(p);
+			
 			System.out.print(p.get_name()+"\t"+p.get_price());
-		
-		
-		// Final order
-		// Summ
-		// Print LCD and Printer	
+			
+			System.out.print("\nQuantity: ");
+			set_amount(scan.nextDouble());
+			
+			//System.out.print(get_amount(0));
+			set_summ(get_summ()+temp.get(0).get_price()*get_amount(0));
+			System.out.print("\nSumm: "+get_summ());
+			
+			scan.close();
+				
 	}	
 }
